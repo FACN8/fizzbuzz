f3b39ee72660f02d0e9d41e9ab22c50059711b87
Jamie Coe <jamiecoe66@gmail.com>
2020-01-07 15:38:20 +0200

    add instruction to delete default test

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var fizzbuzz = require('./index.js');
 
 describe('fizzbuzz', function() {
+  // delete this test after you've checked it passes & Jest is working properly
   it('should work with a simple calculation', function() {
     expect(1 + 1).toBe(2)
   });
