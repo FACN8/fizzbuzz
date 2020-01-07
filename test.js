3f44177ce6444417937dfa9aefeef63cbc2f1bfd
Jamie Coe <jamiecoe66@gmail.com>
2020-01-07 14:39:38 +0200

    replace tape with jest

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-var test = require('tape');
 var fizzbuzz = require('./index.js');
 
-test('Testing Tape is working', function(t) {
-  t.equal(1, 1, 'One should equal one');
-  t.end();
+describe('fizzbuzz', function() {
+  it('should work with a simple calculation', function() {
+    expect(1 + 1).toBe(2)
+  });
 });
